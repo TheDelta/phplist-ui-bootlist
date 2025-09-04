00776a54aa97561bfe76a0a6d3f4ce2e245d27b5
Alex Diller <a.diller@dolphbit.de>
2025-09-04 09:50:20 +0200

    add ugx-mods changes & improvements

--- frontendfooter.php
+++ frontendfooter.php
@@ -5,25 +5,48 @@
 </div><!-- ENDOF #container -->
 
 <div id="footer">
-<div id="footerframe">
-<ul class="list-unstyled">
-<li>&nbsp;</li>
-</ul>
-</div>
+    <div class="px-4">
+        All Rights Reserved - &copy; 2012 - <span id="ugx-footer-year">2025</span> UGX-Mods<br />
+        <a href="https://www.ugx-mods.com/legal-notice" target="_blank" title="Legal Notice">Legal Notice</a> -
+        <a href="https://www.ugx-mods.com/privacy-policy" target="_blank" title="Privacy Policy">Privacy Policy</a>
+    </div>
+    <div id="footerframe">
+        <ul class="list-unstyled">
+            <li>&nbsp;</li>
+        </ul>
+    </div>
 </div>
 
 <script type="text/javascript" src="admin/ui/phplist-ui-bootlist/js/jquery.min.js"></script>
 <script type="text/javascript" src="admin/js/phplistapp.js"></script>
 <script type="text/javascript" src="admin/ui/phplist-ui-bootlist/js/dist/phpList_ui_bootlist.min.js"></script>
 <script>
-/* do not remove this from here */
-$(document).ready(function(){
-    if ( $('body').hasClass('invisible') ){
-        applyCustomFormatting(); applyJqueryUiTabMigration();
-    }
-	$('#edit_list_categories input.form-control').tagsinput('refresh');
+    /* do not remove this from here */
+    $(document).ready(function () {
+        if ($('body').hasClass('invisible')) {
+            applyCustomFormatting(); applyJqueryUiTabMigration();
+        }
+        $('#edit_list_categories input.form-control').tagsinput('refresh');
+        $('div.radio').each(function () {
+            const $el = $(this);
+            if ($el.hasClass('radio-inline')) {
+                $el.addClass('is-inline-fix').removeClass('radio-inline');
+            }
+            $el.addClass('ugx-radio-fix').removeClass('radio');
+        });
+        $('li.list.form-group label.btn.btn-default').each(function () {
+            const $el = $(this);
+            $el.removeClass('btn').removeClass('btn-default');
 
-});
+            const $glyph = $el.children('span');
+            if ($glyph.length) {
+                $glyph.addClass($glyph.hasClass('glyphicon-ok') ? 'text-success' : 'text-danger');
+            }
+        });
+        $('body.fixed .required').removeClass('text-danger').addClass('text-warning');
+        $('body.fixed div.error .required').text(function (i, old) { return old.replace(' red', ''); });
+        $('#ugx-footer-year').text((new Date()).getFullYear());
+    });
 </script>
 </body>
 
--- gruntfile.js
+++ gruntfile.js
@@ -1,62 +1,126 @@
-module.exports = function(grunt) {
-  require('jit-grunt')(grunt);
+module.exports = function (grunt) {
+  require("jit-grunt")(grunt);
 
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    	 concat: {
-            options: {
-                separator: ';'
-            },
-            dist: {
-                src: ['js/jquery.toggleText.js'
-                    ,'js/bootstrap-select.js'
-                    ,'js/phpList3ToBootstrap.js'
-                    ,'bootstrap/dist/js/bootstrap.min.js'
-                    ,'js/bootstrap-tagsinput.js'
-                    ,'js/bootstrap-dialog.js'
-                    ,'js/bootstrap-toggle.js'
-                    ,'js/phplist.js'],
-                dest: 'js/dist/<%= pkg.name %>.js'
-            }
-        },
+    pkg: grunt.file.readJSON("package.json"),
+    concat: {
+      options: {
+        separator: ";",
+      },
+      dist: {
+        src: [
+          "js/jquery.toggleText.js",
+          "js/bootstrap-select.js",
+          "js/phpList3ToBootstrap.js",
+          "bootstrap/dist/js/bootstrap.min.js",
+          "js/bootstrap-tagsinput.js",
+          "js/bootstrap-dialog.js",
+          "js/bootstrap-toggle.js",
+          "js/phplist.js",
+        ],
+        dest: "js/dist/<%= pkg.name %>.js",
+      },
+    },
     uglify: {
       options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n',
       },
       dist: {
         files: {
-          'js/dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
-        }
-      }
+          "js/dist/<%= pkg.name %>.min.js": ["<%= concat.dist.dest %>"],
+        },
+      },
     },
     less: {
       development: {
         options: {
           compress: true,
           yuicompress: true,
-          optimization: 2
+          optimization: 2,
         },
         files: {
-          "css/style.css": "less/style.less" // destination file and source file
-        }
-      }
+          "css/style.css": "less/style.less", // destination file and source file
+        },
+      },
     },
     watch: {
       styles: {
-        files: ['less/**/*.less'], // which files to watch
-        tasks: ['less'],
+        files: ["less/**/*.less"], // which files to watch
+        tasks: ["less", "newer:copy"],
         options: {
-          nospawn: true
-        }
-      }
-    }
+          nospawn: true,
+        },
+      },
+    },
   });
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.registerTask('mydefault', [
-        'less'
-        , 'watch'
-        , 'concat'
-        , 'uglify'
-    ]);
+  grunt.loadNpmTasks("grunt-contrib-concat");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.registerTask("mydefault", ["less", "watch", "concat", "uglify"]);
+  setupUGX(grunt);
 };
+
+function setupUGX(grunt) {
+  grunt.config.set("clean", {
+    build: {
+      files: [{ src: "build/" }],
+    },
+  });
+  grunt.config.set("copy", {
+    build: {
+      files: [
+        {
+          expand: true,
+          dot: true,
+          src: [
+            "**/*",
+            "!.git/**",
+            "!.*",
+            "!package.json",
+            "!package-lock.json",
+            "!composer.json",
+            "!gruntfile.js",
+            "!node_modules/**",
+            "!less/**",
+            "!bootstrap/**",
+            "!.vscode/**",
+            "!js/*.js",
+            "!js/dist/phpList_ui_bootlist.js",
+          ],
+          dest: "build/",
+        },
+      ],
+    },
+  });
+  grunt.config.set("replace", {
+    paths: {
+      src: ["build/**/*.{css,js,php,inc}"],
+      overwrite: true,
+      replacements: [
+        {
+          from: "ui/phplist-ui-bootlist/",
+          to: "ui/ugxmods/",
+        },
+      ],
+    },
+    info: {
+      src: ["build/theme_info"],
+      overwrite: true,
+      replacements: [
+        {
+          from: "name=Trevelin",
+          to: "name=UGXMODS",
+        },
+        {
+          from: "dir=phplist-ui-bootlist",
+          to: "dir=ugxmods",
+        },
+      ],
+    },
+  });
+  grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.loadNpmTasks("grunt-text-replace");
+  grunt.loadNpmTasks("grunt-newer");
+
+  grunt.registerTask("default", ["less", "concat", "uglify", "clean", "copy", "replace"]);
+}
